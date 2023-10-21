1411844cf7884384bec4cb83919410b713c968a7
Umair Saleem <saleemakhtar35302@gmail.com>
2023-10-21 16:25:17 +0500

    add more eslint configurations

---
+++ .eslintrc.cjs
@@ -0,0 +1,63 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:import/typescript',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json', './tsconfig.eslint.json'],
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'import/no-unresolved': 'error',
+    // "@typescript-eslint/no-explicit-any": "off",
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    // "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/camelcase': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': 'warn',
+    camelcase: 'off',
+    'spaced-comment': 'error',
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'import/newline-after-import': ['error', { count: 1 }],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: true,
+        variables: true,
+        typedefs: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: true,
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+};
